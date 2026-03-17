0f3c390eda77d4ffe0ed66c54b5bad9c90e077bf
MariaJoseConceicao <maria.j.b.conceicao1311@gmail.com>
2026-03-17 15:31:58 +0000

    .

---
+++ GestaoTalentos.Shared/DTOs/UpdateSkillDto.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Net.Http.Json;
+namespace GestaoTalentos.Shared.DTOs;
+
+public class UpdateSkillDto
+{
+    public string Nome { get; set; } = string.Empty;
+    public int AreaId { get; set; }
+}
